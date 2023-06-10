3dcabc69753c138cb2b860f8f527d3ac40d12919
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-06-09 21:30:35 -0700

    27

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
+  constructor(private http : HttpClient) {}
 
-  constructor() { }
+  getMovies() {
+    return this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=22deddfff5f1176f137fa66272f529e8");
+  }
 }
