3f83ce49a48e2528b3130f3c20ccab81cb4b6ebd
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-06-10 01:41:35 -0700

    39

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from '../../models/movie';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [animate('1s')]),
+      transition('* => void', [animate('1s')]),
+    ])
+  ]
 })
 export class SliderComponent {
   @Input() items: Movie[] = [];
