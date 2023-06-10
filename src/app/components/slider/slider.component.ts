b84eb1779f8bc38c58659eea84f985acd2d33dd4
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-06-10 00:56:54 -0700

    34

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'slider',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
+  @Input() items: any;
 
 }
