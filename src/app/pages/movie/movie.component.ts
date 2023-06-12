407eae6cd0ae4ed3c7bbbaa587739654f98c738a
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-06-12 00:02:05 -0700

    60

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.scss']
+  styleUrls: ['./movie.component.scss'],
 })
 export class MovieComponent {
-
+  //create a service
+  constructor(private route: ActivatedRoute) {}
+  //when I go to the movie page(movie component) I will call this,read id for url
+  ngOnInit(): void {
+    this.route.params.subscribe(({id}) => {
+      console.log(id);
+    });
+  }
 }
