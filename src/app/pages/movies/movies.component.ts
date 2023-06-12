fe29602b9d7e5970fff49b8a6e1e91173e502762
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-06-11 20:47:27 -0700

    53

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent {
-
+  movies: Movie[] = [];
+  constructor(private movieService: MoviesService) {}
+  ngOnInit(): void {
+    this.movieService.getMovies('popular', 20).subscribe((movies) => {
+      this.movies = movies;
+    });
+  }
 }
