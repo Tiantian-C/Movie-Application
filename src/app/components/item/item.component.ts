5256dc3f352b674e5378b44ca3d68921e4d507cb
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-06-11 18:58:54 -0700

    47

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent {
-
+  @Input() itemData: Movie | null = null;
 }
